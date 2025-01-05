26e43f7605160cb86ee8c75a9f594862e8de0b02
Ruann <ruann.farrapo@indicium.tech>
2025-01-05 15:39:56 -0300

    Fazendo a construção da DAG do pipeline

---
+++ adventure_works_airflow/dags/dbt_pipeline_dag.py
@@ -0,0 +1,42 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+
+# Configurações básicas da DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+}
+
+with DAG(
+    'dbt_pipeline_dag',
+    default_args=default_args,
+    description='Executa a modelagem em dbt diariamente',
+    schedule_interval='0 6 * * *',  # Executa todos os dias às 06:00
+    start_date=days_ago(1),  # Sempre próximo da data atual sem ajustes manuais.
+    catchup=False,
+) as dag:
+
+    # Baixa dependências do dbt
+    dbt_deps = BashOperator(
+        task_id='dbt_deps',
+        bash_command='cd /mnt/d/Documents/Desafios_Indicium/LH_AW_RUANN_CAMPOS/adventure_works_dbt_modeling && dbt deps'
+    )
+
+    # Testa os modelos dbt
+    dbt_test = BashOperator(
+        task_id='dbt_test',
+        bash_command='cd /mnt/d/Documents/Desafios_Indicium/LH_AW_RUANN_CAMPOS/adventure_works_dbt_modeling && dbt test'
+    )
+
+    # Executa as transformações dbt
+    dbt_run = BashOperator(
+        task_id='dbt_run',
+        bash_command='cd /mnt/d/Documents/Desafios_Indicium/LH_AW_RUANN_CAMPOS/adventure_works_dbt_modeling && dbt run'
+    )
+
+    # Dependências entre as tarefas
+    dbt_deps >> dbt_test >> dbt_run
